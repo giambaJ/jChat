bbc39cd15302e307cf84f8f53a00b7fb24d12c23
elmarceloc <73359763+elmarceloc@users.noreply.github.com>
2021-10-22 08:52:06 -0300

    Update script.js

--- v2/script.js
+++ v2/script.js
@@ -1,4 +1,20 @@
+(function($) { // Thanks to BrunoLM (https://stackoverflow.com/a/3855394)
+    $.QueryString = (function(paramsArray) {
+        let params = {};
 
+        for (let i = 0; i < paramsArray.length; ++i) {
+            let param = paramsArray[i]
+                .split('=', 2);
+
+            if (param.length !== 2)
+                continue;
+
+            params[param[0]] = decodeURIComponent(param[1].replace(/\+/g, " "));
+        }
+
+        return params;
+    })(window.location.search.substr(1).split('&'))
+})(jQuery);
 
 const fonts = [ 'BalooTammudu', 'SegoeUI', 'Roboto', 'Lato', 'NotoSans', 'SourceCodePro',
 'Impact', 'Comfortaa', 'DancingScript', 'IndieFlower', 'PressStart2P', 'Wallpoet']
