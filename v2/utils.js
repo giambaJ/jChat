d9d5bf6307e8142dc2f770d0e613c259af03a0b6
elmarceloc <73359763+elmarceloc@users.noreply.github.com>
2021-10-22 08:51:40 -0300

    Update utils.js

--- v2/utils.js
+++ v2/utils.js
@@ -1,21 +1,3 @@
-(function($) { // Thanks to BrunoLM (https://stackoverflow.com/a/3855394)
-    $.QueryString = (function(paramsArray) {
-        let params = {};
-
-        for (let i = 0; i < paramsArray.length; ++i) {
-            let param = paramsArray[i]
-                .split('=', 2);
-
-            if (param.length !== 2)
-                continue;
-
-            params[param[0]] = decodeURIComponent(param[1].replace(/\+/g, " "));
-        }
-
-        return params;
-    })(window.location.search.substr(1).split('&'))
-})(jQuery);
-
 function appendCSS(file){
     $("<link/>", {
         rel: "stylesheet",
