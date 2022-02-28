503ce5ae0f50ea5498665782d69ee562ef6ad51d
arrowgent <57545549+arrowgent@users.noreply.github.com>
2022-02-27 18:46:45 -0800

    remove kraken v5, change to helix, requires oauth
    credentials.js required
    const credentials = 'YOUR_OAUTH_CODE';
    is required to get user_id#
    
    this is a working stop gap for running jchat locally.  has nothing to do with using giamba's website url
    using giamba's website url currently working because his site offers different scripts than posted here
    he is using his own oauth backend to interact with Helix API
    waiting version 3 to be updated here

---
+++ v2/credentials.js
@@ -0,0 +1,12 @@
+/* client_id is not required if running for your own chat.  client_id required for interacting with OTHER's chat */
+const client_id = 'YOUR_CLIENT_ID_CODE';
+const credentials = 'YOUR_OAUTH_CODE';
+// YOUR_USER_ID#
+
+/* references and how to get oauth codes for helix api
+https://dev.twitch.tv/docs/authentication/getting-tokens-oauth/
+https://dev.twitch.tv/docs/authentication
+https://dev.twitch.tv/docs/api/reference
+*/
+
+/* helix oauth codes have a timelimit, 60 days, refreshing codes will be required */
