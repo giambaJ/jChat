c7822be9e3a2091e78f9faa222b1105b2c99ae96
arrowgent <57545549+arrowgent@users.noreply.github.com>
2022-02-27 18:38:05 -0800

    remove kraken v5, change to helix, requires oauth
    credentials.js required
    
    this is a working stop gap for running jchat locally.  has nothing to do with using giamba's website url
    
    using giamba's website url currently working because his site offers different scripts than posted here
    waiting version 3 to be updated here

--- v2/utils.js
+++ v2/utils.js
@@ -19,5 +19,18 @@ function escapeHtml(message) {
 }
 
 function TwitchAPI(url) {
-    return $.getJSON(url + (url.search(/\?/) > -1 ? '&' : '?') + 'client_id=' + client_id);
+/*    return $.getJSON(url + (url.search(/\?/) > -1 ? '&' : '?') + 'client_id=' + client_id);*/
+/* use only helix api oauth endpoint to get user_id and store the id# */
+    return $.ajax({
+        type: "GET", 
+        url: "https://id.twitch.tv/oauth2/validate", 
+        dataType: "json",
+        headers: {'Authorization': 'Bearer ' + credentials},
+/* not required client_id auth for running locally */
+/*        headers: {'Client-Id': client_id},*/
+        success : function(result) { 
+            //set your variable to the result
+            console.log('jChat: helix json aquired user_id');
+        }
+    });
 }
