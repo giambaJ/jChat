8d4aded7d40c63295094aa67f48f70bed58d291b
Juliette Cordor <grassojames5@gmail.com>
2022-11-15 10:32:42 +1100

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+use std::path::PathBuf;
+
+use actix_files::NamedFile;
+use actix_web::{HttpRequest, Result};
+
 #[tokio::main]
-async fn main() -> anyhow::Result<()>{
+async fn main() -> anyhow::Result<()> {
+    use actix_web::{web, App, HttpServer};
+
     println!("Hello, world!");
 
     Ok(())
