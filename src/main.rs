f534f403b59b097d36594582523bf91f91bdb104
Juliette Cordor <grassojames5@gmail.com>
2022-11-15 10:34:25 +1100

    fix

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,17 @@ use std::path::PathBuf;
 use actix_files::NamedFile;
 use actix_web::{HttpRequest, Result};
 
+#[actix_web::get("/twitch/{filename:.*}")]
+async fn twitch(req: HttpRequest) -> Result<NamedFile> {
+    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
+    Ok(NamedFile::open(path)?)
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    use actix_web::{web, App, HttpServer};
+    use actix_web::{App, HttpServer};
 
-    HttpServer::new(|| App::new().route("/{filename:.*}", web::get().to(index)))
+    HttpServer::new(|| App::new().service(twitch))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await?;
