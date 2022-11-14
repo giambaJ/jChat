1126569fc7dc88cd4b2f6ab1fc35a6d68f8b0640
Juliette Cordor <grassojames5@gmail.com>
2022-11-15 10:46:42 +1100

    fixed serving of chat

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,5 @@
 use actix_files::NamedFile;
-use actix_web::{middleware, HttpRequest, Result};
-
-#[actix_web::get("/twitch/")]
-async fn twitch_root() -> Result<NamedFile> {
-    let base_path = std::env::current_dir().unwrap();
-    let path = "index.html";
-
-    let qualified_path = base_path.join("chat").join(path);
-
-    Ok(NamedFile::open(qualified_path)?)
-}
+use actix_web::{HttpRequest, Result};
 
 #[actix_web::get("/twitch/{filename:.*}")]
 async fn twitch(req: HttpRequest) -> Result<NamedFile> {
@@ -33,15 +23,10 @@ async fn twitch(req: HttpRequest) -> Result<NamedFile> {
 async fn main() -> anyhow::Result<()> {
     use actix_web::{App, HttpServer};
 
-    HttpServer::new(|| {
-        App::new()
-            .service(twitch)
-            .service(twitch_root)
-            .wrap(middleware::NormalizePath::default())
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await?;
+    HttpServer::new(|| App::new().service(twitch))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await?;
 
     Ok(())
 }
