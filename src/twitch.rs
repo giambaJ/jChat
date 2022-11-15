cce9133674526bce5590436b130bdc0961239586
Juliette Cordor <grassojames5@gmail.com>
2022-11-15 11:13:39 +1100

    add twitch api defs

---
+++ src/twitch.rs
@@ -0,0 +1,30 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TwitchUsers {
+    pub total: i64,
+    pub data: Vec<Datum>,
+    pub pagination: Pagination,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Datum {
+    pub from_id: String,
+    pub from_login: String,
+    pub from_name: String,
+    pub to_id: String,
+    pub to_login: To,
+    pub to_name: To,
+    pub followed_at: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Pagination {
+    pub cursor: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum To {
+    #[serde(rename = "sapphicjewl")]
+    Sapphicjewl,
+}
