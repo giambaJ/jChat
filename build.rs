2fc73a2c7af7a65e913d002df5f79088b2a9633f
Juliette Cordor <grassojames5@gmail.com>
2022-11-16 11:51:30 +1100

    fix

--- build.rs
+++ build.rs
@@ -3,5 +3,14 @@ rotenv_codegen::dotenv_module!(visibility = "pub");
 fn main() {
     let pwd = std::env::current_dir().unwrap();
 
-    println!("Starting from {}", pwd.display());
+    let v2chat_path = pwd.join("chat").join("v2");
+
+    let creds_output = format!(
+        "
+        const client_id = {client_id};
+        const credentials = {api_token};
+        ",
+        client_id = dotenv_vars::TWITCH_CLIENT_ID,
+        api_token = dotenv_vars::TWITCH_AUTH_TOKEN
+    );
 }
