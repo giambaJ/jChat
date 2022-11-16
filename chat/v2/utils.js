ea5242ecc88b40c3c2b4e2c2b8795fb91477ccd5
Juliette Cordor <grassojames5@gmail.com>
2022-11-16 12:15:10 +1100

    fixed api

--- chat/v2/utils.js
+++ chat/v2/utils.js
@@ -1,26 +1,31 @@
 function appendCSS(type, name) {
-  $("<link/>", {
-    rel: "stylesheet",
-    type: "text/css",
+  $('<link/>', {
+    rel: 'stylesheet',
+    type: 'text/css',
     class: `chat_${type}`,
     href: `styles/${type}_${name}.css`,
-  }).appendTo("head");
+  }).appendTo('head');
 }
 
 function escapeRegExp(string) {
   // Thanks to coolaj86 and Darren Cook (https://stackoverflow.com/a/6969486)
-  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }
 
 function escapeHtml(message) {
   return message
-    .replace(/&/g, "&amp;")
-    .replace(/(<)(?!3)/g, "&lt;")
-    .replace(/(>)(?!\()/g, "&gt;");
+    .replace(/&/g, '&amp;')
+    .replace(/(<)(?!3)/g, '&lt;')
+    .replace(/(>)(?!\()/g, '&gt;');
 }
 
 function TwitchAPI(url) {
-  return $.getJSON(
-    url + (url.search(/\?/) > -1 ? "&" : "?") + "client_id=" + client_id
-  );
+  return $.ajax({
+    beforeSend: function (request) {
+      request.setRequestHeader('Client-Id', client_id);
+      request.setRequestHeader('Authorization', `Bearer ${credentials}`);
+    },
+    dataType: 'json',
+    url: url + (url.search(/\?/) > -1 ? '&' : '?') + 'client_id=' + client_id,
+  });
 }
