10898d0ebed4cb3fff462f943c8b87f58e0d5319
Juliette Cordor <grassojames5@gmail.com>
2022-11-23 14:07:04 +1100

    fix

--- chat/v2/script.js
+++ chat/v2/script.js
@@ -603,9 +603,13 @@ Chat = {
 
     Chat.load(function () {
       console.log('jChat: Connecting to IRC server...');
-      var socket = new ReconnectingWebSocket('ws://127.0.0.1:8080/ws/', 'irc', {
-        reconnectInterval: 2000,
-      });
+      var socket = new ReconnectingWebSocket(
+        'ws://127.0.0.1:8080/ws/',
+        'websocket',
+        {
+          reconnectInterval: 2000,
+        },
+      );
 
       socket.onopen = function () {
         console.log('jChat: Connected');
