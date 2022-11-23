7b58a955f7b877f88f1a3e7c6682bc990db7665b
Juliette Cordor <grassojames5@gmail.com>
2022-11-23 20:29:41 +1100

    fid

--- src/creds.rs
+++ src/creds.rs
@@ -13,3 +13,21 @@ pub const CREDENTIALS: Credentials = Credentials {
     auth_token: env!("TWITCH_AUTH_TOKEN"),
     refresh_token: env!("TWITCH_REFRESH_TOKEN"),
 };
+
+impl Credentials {
+    pub async fn expires_in(&self) -> anyhow::Result<u64> {
+        use crate::twitch_api::CLIENT;
+
+        let response: serde_json::Value = CLIENT
+            .get("https://id.twitch.tv/oauth2/validate")
+            .await?
+            .json()
+            .await?;
+
+        let expires_in = response["expires_in"].as_u64().ok_or_else(|| {
+            anyhow::anyhow!("Could not parse expires_in from response: {:?}", response)
+        })?;
+
+        expires_in
+    }
+}
