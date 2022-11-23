c1216b3067ebd7f70c8a74b5df55454d6ef5fd60
Juliette Cordor <grassojames5@gmail.com>
2022-11-23 20:52:37 +1100

    fix

--- src/irc/actor.rs
+++ src/irc/actor.rs
@@ -12,28 +12,32 @@ pub struct FakeIrc {
     pub addrs: Vec<Recipient<Message>>,
 }
 
+const MSG: & str = "@badge-info=subscriber/22;badges=broadcaster/1,subscriber/3012;client-nonce=6090b7621f1bf7bdcc46777cd522bca1;color=#29DE7A;display-name=sapphicjewl;emotes=;first-msg=0;flags=;id=aedfa462-66b6-4a2b-b94d-afb01d0631f9;mod=0;returning-chatter=0;room-id=538134305;subscriber=1;tmi-sent-ts=1668563455712;turbo=0;user-id=538134305;user-type= :sapphicjewl!sapphicjewl@sapphicjewl.tmi.twitch.tv PRIVMSG #sapphicjewl :monkaS\r\n";
+
 impl Actor for FakeIrc {
     type Context = ws::WebsocketContext<Self>;
 
     fn started(&mut self, ctx: &mut Self::Context) {
-        ctx.run_interval(Duration::from_secs(5), |server, _| {
-            server.send_message("@badge-info=subscriber/22;badges=broadcaster/1,subscriber/3012;client-nonce=6090b7621f1bf7bdcc46777cd522bca1;color=#29DE7A;display-name=sapphicjewl;emotes=;first-msg=0;flags=;id=aedfa462-66b6-4a2b-b94d-afb01d0631f9;mod=0;returning-chatter=0;room-id=538134305;subscriber=1;tmi-sent-ts=1668563455712;turbo=0;user-id=538134305;user-type= :sapphicjewl!sapphicjewl@sapphicjewl.tmi.twitch.tv PRIVMSG #sapphicjewl :monkaS\r\n");
-        });
-    }
-}
+        ctx.run_interval(Duration::from_secs(5), |server, ctx| {
+            info!("Sending message");
+            for addr in &server.addrs {
+                addr.send(Message(MSG.to_owned()))
+                    .into_actor(server)
+                    .then(|res, _, ctx| {
+                        match res {
+                            Ok(_) => (),
+                            Err(e) => {
+                                error!("Error sending message: {}", e);
+                                ctx.stop();
+                            }
+                        }
+                        fut::ready(())
+                    })
+                    .wait(ctx);
 
-impl FakeIrc {
-    /// Send message to all users in the room
-    fn send_message(&self, message: &str) {
-        info!("Sending message");
-        for addr in &self.addrs {
-            let rt = tokio::runtime::Handle::current();
-
-            rt.block_on(async {
-                addr.send(Message(message.to_owned())).await.unwrap();
                 info!("Response gotten");
-            });
-        }
+            }
+        });
     }
 }
 
