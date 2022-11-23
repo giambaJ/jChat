ca211abc92c31f1194400c1b096221b15a58dbb0
Juliette Cordor <grassojames5@gmail.com>
2022-11-23 13:25:19 +1100

    fix

--- src/irc.rs
+++ src/irc.rs
@@ -1,5 +1,10 @@
-use irc::server;
+use actix_web::{web, Error, HttpRequest, HttpResponse};
+use actix_web_actors::ws;
 
 mod actor;
 
-pub static IRC_CONFIG: Config = Config {};
+pub async fn handle_ws(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let resp = ws::start(actor::FakeIrc, &req, stream);
+    println!("{:?}", resp);
+    resp
+}
