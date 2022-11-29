9e6b8225600d0d1ff49279c172d9cab2c6f72616
Juliette Cordor <grassojames5@gmail.com>
2022-11-29 12:01:42 +1100

    fix

--- src/command.rs
+++ src/command.rs
@@ -2,3 +2,11 @@ pub enum Command {
     Write(String, u64),
     Pause(u64),
 }
+
+impl TryFrom<String> for Command {
+    type Error;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        todo!()
+    }
+}
