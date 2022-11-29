0c4a85c6c6ec0bbec7039821a871c4d37a1a305f
Juliette Cordor <grassojames5@gmail.com>
2022-11-29 12:07:34 +1100

    fix

--- src/command.rs
+++ src/command.rs
@@ -2,6 +2,8 @@
 pub enum CommandError {
     #[error("The command provided was invalid. Found {0}")]
     InvalidCommand(String),
+    #[error("No command was provided")]
+    MissingCommand,
     #[error("The number provided was invalid")]
     InvalidNumber,
 }
@@ -17,6 +19,13 @@ impl TryFrom<String> for Command {
     fn try_from(value: String) -> Result<Self, Self::Error> {
         if let Some(command) = value.strip_prefix("/") {
             let split = command.split_whitespace().collect::<Vec<_>>();
+
+            match split.get(0) {
+                Some("write") => {}
+                Some("pause") => {}
+                Some(x) => Err(Self::Error::InvalidCommand(x)),
+                None => Err(Self::Error::MissingCommand),
+            }
         } else {
             Ok(Self::Write(value, 1))
         }
